c578de09ad1c3fa95d2af7834cbebf299cc7a584
bhauguel <baptiste.hauguel@photobox.com>
2020-09-15 18:15:36 +0200

    add ISymptomReader

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -0,0 +1,21 @@
+package com.hemebiotech.analytics;
+
+import java.util.Map;
+
+/**
+ * Anything that will read symptom data from a source
+ * The important part is, the return value from the operation, which is a list of strings,
+ * that may contain many duplications
+ * 
+ * The implementation does not need to order the list
+ * 
+ */
+public interface ISymptomReader {
+	/**
+	 * If no data is available, return an empty List
+	 * 
+	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
+	 */
+	Map<String,Integer> GetSymptoms ();
+	void saver (Map<String,Integer> data);
+}
